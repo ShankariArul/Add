3b59936b2f8e9cc87f719915a81e13822442b151
shankari <shankari@localhost>
2023-06-29 16:26:50 -0700

    Uploading proj

---
+++ src/Add/Addition.java
@@ -0,0 +1,15 @@
+package Add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int first = 10;
+	    int second = 20;
+
+	    
+	    int sum = first + second;
+	    System.out.println(first + " + " + second + " = "  + sum);
+
+	}
+
+}
